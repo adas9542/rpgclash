99859bb0d5be9ed87a3e70f220e3df49c65b8fde
Arnab <dazarnab@gmail.com>
2020-01-12 17:10:38 -0500

    Player Basics

---
+++ classes/main.py
@@ -0,0 +1,10 @@
+from classes.game import Person, bcolors
+
+magic = [{"name": "Fire", "cost": 15, "dmg": 70},
+{"name": "Ice", "cost": 13, "dmg": 65},
+{"name": "Lightning", "cost": 16, "dmg": 67},
+{"name": "Poison", "cost": 12, "dmg": 62},
+]
+
+player = Person(400, 70, 71, 32, magic)
+
