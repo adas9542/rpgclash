c3127003279325a9c01b1fe05bfdc8d46c8ea77c
Arnab <dazarnab@gmail.com>
2020-01-19 02:54:37 -0500

    Improve format

--- classes/main.py
+++ classes/main.py
@@ -1,36 +1,103 @@
 from classes.game import Person, bcolors
-import random
+from classes.magic import Spell
 
-magic = [{"name": "Fire", "cost": 15, "dmg": 70},
-{"name": "Ice", "cost": 13, "dmg": 65},
-{"name": "Lightning", "cost": 16, "dmg": 67},
-{"name": "Poison", "cost": 12, "dmg": 62},
-]
+import random
+import math
+
+fire = Spell("Fire", 25, 370, "Black")
+ice = Spell("Ice", 13, 265, "Black")
+lightning = Spell("Lightning", 16, 267, "Black")
+poison = Spell("Poison", 12, 250, "Black")
+cure = Spell("Cure", 15, 70, "White")
+
+magic = [{"name": fire.name, "cost": fire.cost, "dmg": fire.dmg, "type": fire.type},
+         {"name": ice.name, "cost": ice.cost, "dmg": ice.dmg, "type": ice.type},
+         {"name": lightning.name, "cost": lightning.cost, "dmg": lightning.dmg, "type": lightning.type},
+         {"name": poison.name, "cost": poison.cost, "dmg": poison.dmg, "type": poison.type},
+         {"name": cure.name, "cost": cure.cost, "dmg": cure.dmg, "type": cure.type}]
 
 player = Person(400, 70, 71, 32, magic)
 
 enemy = Person(1200, 65, 45, 25, magic)
 
 i = 1
-while 1:
-    print("=======Your Turn=========")
+
+
+def mp_valid(player_mp, mp_cost):
+    return int(player_mp) - mp_cost
+
+
+def stats_calc(x, spell_cost, damage, type):
+    if (x == 2 and type == "Black"):
+        player.mp -= spell_cost
+        enemy.take_dmg(damage)
+    elif (x == 1 and type == "Black"):
+        enemy.take_dmg(damage)
+    elif (x == 2 and type == "Black" and player.maxhp != player.mp):
+        player.hp += damage
+
+
+def act_statement():
+    print("==============Your Turn==================")
+    player.choose_action()
+    act = input(bcolors.OKBLUE + "Which action do you choose? : " + bcolors.ENDC)
+    return act
+
+
+def magic_option():
     player.choose_magic()
-    choice = input("Which magic do you choose? : ")
-    upd_option = int(choice) - 1
-    print("You chose " + player.get_spell_name( upd_option ))
-    damage_inflicted = player.gen_spelldmg(upd_option)
-    enemy.take_dmg(damage_inflicted)
-    print("Enemy has lost ", damage_inflicted, " hp1. Enemy hp: ", enemy.hp)
+    choice = input(bcolors.OKBLUE + "Which magic do you choose? : " + bcolors.ENDC)
+    upd_magic = int(choice) - 1
+    return upd_magic
 
-    print("==============Enemy's Turn================")
-    enemy.choose_magic()
-    enemy_choice = enemy.gen_spelldmg(random.randrange(0,4))
-    enemy_damage = int(enemy_choice)
-    player.take_dmg(enemy_damage)
-    print("You has lost ", enemy_damage, " hp1. Your hp: ", player.hp)
 
+def domagic(option, valid):
+    if int(valid) > 0:  # use magic spell
+        damage_inflicted = player.gen_spelldmg(option)
+        spell_cost = player.spell_mp_cost(option)
+        spell_name = player.get_spell_name(option)
+        type = player.get_type(option)
+        stats_calc(int(action), spell_cost, damage_inflicted, type)
+        print("You chose " + spell_name)
+        return damage_inflicted
+    elif int(valid) <= 0:
+        print(bcolors.BOLD + "You don't have enough magic points" + bcolors.ENDC)
+
+
+def doattack():
+    spell_cost = 0
+    damage_inflicted = player.gen_damage()
+    type = "None"
+    stats_calc(int(action), spell_cost, damage_inflicted, type)
+    print(bcolors.BOLD + bcolors.OKGREEN + "You have attacked" + bcolors.ENDC)
+    return damage_inflicted
+
+
+while 1:
+    action = int(act_statement())
+    if action == 2: #cure does not work
+                    #attack by itself does not work
+                    #enemy is unable to reach 0 hp
+        option = magic_option()
+        valid = mp_valid(player.mp, player.spell_mp_cost(option))
+        damage_inflicted = domagic(option, valid)
+
+    if action == 1 or valid <= 0:
+        damage_inflicted = doattack()
+
+    print("Enemy has lost", damage_inflicted, "hp.")
+    print("===============Stats=====================")
+    print("Enemy hp:", enemy.hp, "\nYour hp:", player.hp, "\nYour mp:", player.mp)
+
+    print("=============Enemy's Turn================")
+    enemy_dmg = enemy.gen_damage()
+    player.take_dmg(enemy_dmg)
+    print(bcolors.FAIL + bcolors.BOLD + "Enemy attacks:", "\nYou lost ", enemy_dmg, " hp. You have", player.get_hp(),
+          "hp left.", bcolors.ENDC)
+    if player.hp == 0:
+        print(bcolors.FAIL + bcolors.BOLD + "Battle is Over. You have lost!" + bcolors.ENDC)
+        break
+    if enemy.hp == 0:
+        print(bcolors.OKGREEN + "\nYou have defeated the enemy!" + bcolors.ENDC)
+        break
     i = 0
-
-
-print(bcolors.FAIL + bcolors.BOLD + "Enemy Attacks!" + bcolors.ENDC)
-
